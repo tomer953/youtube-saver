41c32108cd7bb97c97dcc35339ab90a4cc8c270e
Tomer953 <tomer953@gmail.com>
2021-05-02 14:38:49 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const mv = require('mv');
+const ytdl = require('ytdl-core');
 const express = require('express')
 const app = express()
 const cors = require('cors');
@@ -5,9 +8,38 @@ const port = 8080
 
 app.use(cors());
 
-app.get('/save', (req, res) => {
-    console.log('downloading video', req.query.id, decodeURIComponent(req.query.title))
-    setTimeout(() => res.send('Saved'), 1500);
+app.get('/save', (req, res, next) => {
+    try {
+        console.log('downloading video', req.query.id, decodeURIComponent(req.query.title));
+        
+        let id = req.query.id;
+        let title = decodeURIComponent(req.query.title);
+
+        if (!id || !title) {
+            throw new Error("Invalid params");
+        }
+
+        // remove invalid chars from title
+        let videoName = title + '.mp4';
+        videoName = videoName.replace(/[/\\?%*:|"<>]/g, '-');
+
+        let videoUrl = `https://www.youtube.com/watch?v=${id}`;
+
+        // download video
+        let stream = ytdl(videoUrl).pipe(fs.createWriteStream(videoName));
+        stream.on('finish', function () {
+
+            // on finish move to H:\
+            console.log('finish');
+            mv(videoName, 'H:\\' + videoName, (err) => { if (err) console.log(err); });
+            return res.send('ok')
+        });
+
+    } catch (error) {
+        console.log(error);
+        next()
+    }
+
 })
 
 app.listen(port, () => {
